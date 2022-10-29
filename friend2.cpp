0166d785130b23c4aa4b937f3f1144d5d9f37569
thesinceman <theeeveeman1@gmail.com>
2022-10-29 14:40:53 +1300

    added c++ file

---
+++ friend2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void displaymenu()
+{ cout<<"===================================================== \n";
+cout<<" \t\tFRIEND 2!!!\t \n ";
+cout<<"===================================================== \n";
+cout<<" 1.play psr\n";
+cout<<" 2.have a convosation\n";
+cout<<" 3.exit\n";
+}
+int main(){
+    displaymenu();
+    return 0;
+}
